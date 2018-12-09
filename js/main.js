c014ee5790718aeaa81ff03ea49359c452f5985e
pmohan17 <43380900+pmohan17@users.noreply.github.com>
2018-12-09 11:13:21 -0500

    edit 12/8

--- js/main.js
+++ js/main.js
@@ -1,27 +1,16 @@
 $(document).ready(() => {
+  $('body').addClass("firstBackground")
     $('#intro').hide()
-    $('#myBtn').hide()
-    $('.morning').hide()
+    $('.navigation').hide()
 $('.begin').click(() => {
+  $('body').removeClass("firstBackground")
+  $('.navigation').show()
   $('#intro').show()
   $('#myBtn').show()
   $('.begin').hide()
   $('#opening').hide()
   $('#subheading').hide()
-});
-$('#myBtn').click(() => {
-$('#intro').hide()
-$('.morning').show()
-$('#myBtn').hide()
-$('.getDressed').hide()
-$('.outfit1').hide()
-$('.whiteBlock').hide()
 
 });
-$('.scrollDress').click(() => {
-  $('.getDressed').show()
-  $('.outfit1').show()
-  $('.whiteBlock').show()
 
-});
 });
